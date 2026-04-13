0d28d954524b5e5b9d02478a634710ae3f0d737e
Rusydi Noor Abdurrahman <71594097+rusdino@users.noreply.github.com>
2026-04-13 08:41:07 +0700

    feat: Add conditions for entry permissions based on inputs

--- soal1.c
+++ soal1.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
 int main(void) {
+    int izin, suhu, radiasi, jam;
+    scanf("%d %d %d %d", &izin, &suhu, &radiasi, &jam);
+    if(radiasi >= 6){
+        printf("TOLAK");
+    }else if(suhu >= 390){
+        printf("KARANTINA");
+    }else if(izin == 1 && (jam < 6 || jam>20)){
+        printf("TOLAK");
+    }else if(izin == 1){
+        printf("MASUK");
+    }else if(izin == 2 && radiasi <= 2 && jam >= 8 && jam <= 18){
+        printf("MASUK");
+    }else if(izin == 2){
+        printf("PEMERIKSAAN");
+    }else if(izin == 3 && radiasi == 0 && suhu <380){
+        printf("MASUK");
+    }else{
+        printf("TOLAK");
+    }
     return 0;
 }
